e91c6aa971b33b9e95865b5be3d0322fa9b48107
Eli Ribble <eli@gleipnir.technology>
2025-03-10 06:22:18 -0700

    Initial "hello world" code.

---
+++ LokiNotes/LokiNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LokiNotesApp.swift
+//  LokiNotes
+//
+//  Created by Eli Ribble on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct LokiNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
