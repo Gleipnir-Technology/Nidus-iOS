cda152893254b1c33b596987de8aedb68748b03b
Eli Ribble <eli@gleipnir.technology>
2025-03-13 16:00:30 -0700

    Start working on getting previews to work how I'd like
    It's a bit ugly, but this at least shows some of the concepts of getting data through the view model into the UI level

--- Nidus/ContentView.swift
+++ Nidus/ContentView.swift
@@ -7,49 +7,54 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @StateObject var locationDataManager = LocationDataManager()
-    
-    var body: some View {
-        VStack {
-            switch locationDataManager.locationManager.authorizationStatus {
-            case .authorizedWhenInUse: // location services are available.
-                Text("Your current location is:")
-                Text("Latitude: \(locationDataManager.locationManager.location?.coordinate.latitude.description ?? "Error loading")")
-                Text("Longitude: \(locationDataManager.locationManager.location?.coordinate.longitude.description ?? "Error loading")")
-                Text("Precision: \(locationDataManager.locationManager.location?.horizontalAccuracy.formatted() ?? "Error loading")")
-            case .restricted, .denied: // Not available
-                Text("Current location data was restricted or denied.")
-            case .notDetermined: // not determined yet
-                Text("Finding your location...")
-                ProgressView()
-            default:
-                ProgressView()
-            }
-        }
-        /*
-        NavigationSplitView {
-            ZStack {
-                NoteList()
-                VStack {
-                    Spacer()
-                    HStack {
-                        Spacer()
-                        NavigationLink {
-                            NoteCreation()
-                        } label: {
-                            ButtonAddNote()
-                        }
-                    }
-                }
-            }
-            .navigationTitle("Notes")
-        } detail: {
-            Text("Nidus")
-        }*/
-    }
+class ViewModel: ObservableObject {
+	@Published var text: String = "Getting location..."
 }
 
-#Preview {
-    ContentView().environment(ModelData())
+struct ContentView: View {
+	@StateObject var locationDataManager = LocationDataManager()
+	@StateObject private var viewModel: ViewModel
+
+	init(viewModel: ViewModel = ViewModel()) {
+		_viewModel = StateObject(wrappedValue: viewModel)
+	}
+
+	var body: some View {
+		VStack {
+			switch locationDataManager.locationManager.authorizationStatus {
+			case .authorizedWhenInUse:  // location services are available.
+				Text("Your current location is:")
+				Text(
+					"Latitude: \(locationDataManager.locationManager.location?.coordinate.latitude.description ?? "Error loading")"
+				)
+				Text(
+					"Longitude: \(locationDataManager.locationManager.location?.coordinate.longitude.description ?? "Error loading")"
+				)
+				Text(
+					"Precision: \(locationDataManager.locationManager.location?.horizontalAccuracy.formatted() ?? "Error loading")"
+				)
+			case .restricted, .denied:  // Not available
+				Text("Current location data was restricted or denied.")
+			case .notDetermined:  // not determined yet
+				Text(viewModel.text)
+				ProgressView()
+			default:
+				ProgressView()
+			}
+		}
+	}
+}
+
+#Preview("Before Location") {
+	ContentView().environment(ModelData())
+}
+
+#Preview("After Location") {
+	ContentView(
+		viewModel: {
+			let vm = ViewModel()
+			vm.text = "Test Value"
+			return vm
+		}()
+	)
 }
--- Nidus/Views/NoteList.swift
+++ Nidus/Views/NoteList.swift
@@ -11,13 +11,35 @@ struct NoteList: View {
 	@Environment(ModelData.self) var modelData
 
 	var body: some View {
-		List(modelData.notes) { note in
-			NavigationLink {
-				NoteDetail(note: note)
-			} label: {
-				NoteRow(note: note)
-			}
-		}
+        NavigationSplitView {
+            ZStack {
+                if(modelData.notes.count == 0) {
+                    Text("No notes yet")
+                } else {
+                    List(modelData.notes) { note in
+                        NavigationLink {
+                            NoteDetail(note: note)
+                        } label: {
+                            NoteRow(note: note)
+                        }
+                    }
+                }
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            NavigationLink {
+                                NoteCreation()
+                            } label: {
+                                ButtonAddNote()
+                            }
+                        }
+                    }
+                    .navigationTitle("Notes")
+                }
+        } detail: {
+            Text("Nidus")
+        }
 	}
 }
 
