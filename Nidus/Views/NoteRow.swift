c88ab36b7c891e2f2a67dd8c5639d3dd75220afe
Eli Ribble <eli@gleipnir.technology>
2025-03-19 17:15:16 -0700

    Show notes by distance.

--- Nidus/Views/NoteRow.swift
+++ Nidus/Views/NoteRow.swift
@@ -4,15 +4,36 @@
 //
 //  Created by Eli Ribble on 3/11/25.
 //
-
+import CoreLocation
 import SwiftUI
 
 struct NoteRow: View {
+	@Environment(LocationDataManager.self) private var locationDataManager
+
 	var note: Note
+
+	func distanceString() -> String {
+		if let location = locationDataManager.location {
+			if let noteCoord = note.location {
+				let noteLocation = CLLocation(
+					latitude: noteCoord.latitude,
+					longitude: noteCoord.longitude
+				)
+				let distance = location.distance(from: noteLocation)
+				return String(format: "%.1f m", distance)
+			}
+			else {
+				return "No location recorded"
+			}
+		}
+		else {
+			return "Location unavailable"
+		}
+	}
 	var body: some View {
 		HStack {
-			//note.image.resizable().frame(width: 50, height: 50)
-			Text(note.content)
+			Label(note.category.name, systemImage: note.category.icon)
+			Text(distanceString())
 			Spacer()
 		}
 		.padding()
@@ -21,8 +42,6 @@ struct NoteRow: View {
 
 #Preview("note 1") {
 	Group {
-		//NoteRow(note: ModelData().notes[0])
-		//NoteRow(note: ModelData().notes[1])
-		//NoteRow(note: note)
+		NoteRow(note: Note.dog)
 	}
 }
