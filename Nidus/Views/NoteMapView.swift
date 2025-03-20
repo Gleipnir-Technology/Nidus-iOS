3cac4c4cb934a6b741bbf7e9b1ab9834a63089c6
Eli Ribble <eli@gleipnir.technology>
2025-03-20 13:26:19 -0700

    Show a fixed map with a few markers
    We're getting this from https://www.createwithswift.com/using-mapkit-with-swiftui/
    It's using a bunch of deprecated APIs we'll need to convert away from.

--- Nidus/Views/NoteMapView.swift
+++ Nidus/Views/NoteMapView.swift
@@ -7,20 +7,44 @@
 import MapKit
 import SwiftUI
 
-struct NoteMapView: View {
+struct PointOfInterest: Identifiable {
+	let id = UUID()
+	let name: String
+	let latitude: Double
+	let longitude: Double
 
-	//@State var currentLocation: CLLocationCoordinate2D
-	//@State var notes: [Note]
+	var coordinates: CLLocationCoordinate2D {
+		CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+	}
+}
+struct NoteMapView: View {
 	@State private var region = MKCoordinateRegion(
-		center: CLLocationCoordinate2D(latitude: 37.3318, longitude: -121.8863),
+		center: CLLocationCoordinate2D(latitude: 40.83834587046632, longitude: 14.25),
 		span: MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03)
 	)
 
+	private let places = [
+		//2.
+		PointOfInterest(
+			name: "Galeria Umberto I",
+			latitude: 40.83859036140747,
+			longitude: 14.24945566830365
+		),
+		PointOfInterest(name: "Castel dell'Ovo", latitude: 40.828206, longitude: 14.247549),
+		PointOfInterest(
+			name: "Piazza Dante",
+			latitude: 40.848891382971985,
+			longitude: 14.250055428532933
+		),
+	]
+
 	var body: some View {
-		Map(coordinateRegion: $region)
+		Map(coordinateRegion: $region, annotationItems: places) {
+			place in MapMarker(coordinate: place.coordinates)
+		}
 	}
 }
 
-/*#Preview {
-    NoteMapView().environment(LocationDataManagerForPreview())
-}*/
+#Preview("sample", traits: .modifier(MockDataPreviewModifier())) {
+	NoteMapView()
+}
