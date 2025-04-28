df942b4f83b23be0f0c5687295ab4e800f1457fc
Eli Ribble <eli@gleipnir.technology>
2025-04-28 16:23:25 -0700

    Add MapOverview view.

---
+++ Nidus/Views/MapOverview.swift
@@ -0,0 +1,45 @@
+//
+//  MapOverview.swift
+//  Nidus
+//
+//  Created by Eli Ribble on 4/28/25.
+//
+import CoreLocation
+import MapKit
+import SwiftData
+import SwiftUI
+
+struct MapOverview: View {
+	@Query() private var notes: [Note]
+
+	var body: some View {
+		ZStack(alignment: .trailing) {
+			Map {
+				ForEach(notes, id: \.id) { note in
+					Marker(
+						note.category.name,
+						systemImage: note.category.icon,
+						coordinate:
+							note.location.asCLLocationCoordinate2D()
+					).tint(
+						.orange
+					)
+				}
+			}.mapControls {
+				MapCompass()
+				MapScaleView()
+				MapUserLocationButton()
+			}.mapStyle(
+				MapStyle.standard(
+					pointsOfInterest: PointOfInterestCategories.excludingAll
+				)
+			)
+		}
+	}
+}
+
+#Preview("1") {
+	ModelContainerPreview(ModelContainer.sample) {
+		MapOverview()
+	}
+}
