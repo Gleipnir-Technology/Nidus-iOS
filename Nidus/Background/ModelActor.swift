004239ac9ebaa0b05036e6f127283f6563c37ffe
Eli Ribble <eli@gleipnir.technology>
2025-05-28 06:55:28 -0700

    Remove dead-end work on the model actor

--- Nidus/Background/ModelActor.swift
+++ Nidus/Background/ModelActor.swift
@@ -1,4 +1,3 @@
-import OSLog
 //
 //  ModelActor.swift
 //  Nidus Notes
@@ -7,33 +6,6 @@ import OSLog
 //
 import SwiftData
 import SwiftUI
-
-/*
- https://www.massicotte.org/model-actor
-@MainActor
-func makeMainModelActor() -> MyModelActor {
-    MyModelActor(modelContainer: .shared)
-}
-
-func makeBackgroundModelActor() async -> MyModelActor {
-    MyModelActor(modelContainer: .shared)
-}
-
-@ModelActor
-actor MyModelActor {
-    func hello() {
-        print("Hi there!")
-    }
-}
-
-extension ModelActor {
-    func withContext<T>(
-        _ block: (ModelContext) -> T
-    ) -> T {
-        block(modelContext)
-    }
-}
-*/
 
 /* from
  https://useyourloaf.com/blog/swiftdata-background-tasks/
