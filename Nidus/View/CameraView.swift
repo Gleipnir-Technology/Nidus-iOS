d2b8e9efb42ed84021b2e95c162b8f09e455b12c
Eli Ribble <eli@gleipnir.technology>
2025-08-13 09:38:24 -0700

    Simplify the camera view
    It's crashing, and I don't know why. So I removed some stuff to try to
    get it stable.

--- Nidus/View/CameraView.swift
+++ Nidus/View/CameraView.swift
@@ -1,73 +1,40 @@
 import MijickCamera
+import OSLog
 import SwiftUI
 
 /*
  Our own custom camera view with controls.
  */
 struct CameraView: View {
-	@State var controller: CameraController
+	var controller: CameraController
 	// The controller that we'll trigger when the user does stuff
 	let toDismiss: () -> Void
 	// Function to dismiss this control
 
-	let forPreview: Bool
-	// Whether or not we're showing this control as part of a preview. Helps to avoid crashing Previews.
-
 	init(
 		controller: CameraController,
-		forPreview: Bool = false,
 		toDismiss: @escaping () -> Void
 	) {
 		self.controller = controller
-		self.forPreview = forPreview
 		self.toDismiss = toDismiss
 	}
 
 	var body: some View {
-		if forPreview {
-			Spacer()
-			Image(uiImage: UIImage(named: "camera-placeholder")!).resizable()
-				.aspectRatio(contentMode: .fit).background(
-					Color.cyan.opacity(0.4)
-				)
-		}
-		else {
-			MCamera()
-				.onImageCaptured { image, camera in
-					controller.saveImage(image)
-					camera.reopenCameraScreen()
-				}
-				.setCloseMCameraAction {
-					toDismiss()
-				}
-				.onVideoCaptured { url, camera in
-					controller.saveVideo(url)
-					camera.reopenCameraScreen()
-				}.startSession()
-				.navigationBarBackButtonHidden(true)
-		}
-	}
-}
-
-struct CameraControls: View {
-	var body: some View {
-		HStack {
-			Image(systemName: "photo").font(.system(size: 64, weight: .regular))
-				.padding(20)
-			Spacer()
-			Image(systemName: "camera.aperture").font(
-				.system(size: 82, weight: .regular)
-			)
-			Spacer()
-			Image(systemName: "record.circle").font(.system(size: 64, weight: .regular))
-				.padding(20)
-		}
-	}
-}
-
-struct CameraView_Previews: PreviewProvider {
-	@State static var controller: CameraController = CameraControllerPreview()
-	static var previews: some View {
-		CameraView(controller: controller, forPreview: true, toDismiss: {})
+		MCamera()
+			.onError { error in
+				Logger.foreground.error("Camera error: \(error)")
+			}
+			.onImageCaptured { image, camera in
+				controller.saveImage(image)
+				camera.reopenCameraScreen()
+			}
+			.setCloseMCameraAction {
+				toDismiss()
+			}
+			.onVideoCaptured { url, camera in
+				controller.saveVideo(url)
+				camera.reopenCameraScreen()
+			}.startSession()
+		//.navigationBarBackButtonHidden(true)
 	}
 }
