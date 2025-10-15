bc698e3c7e1d943ec5722bfc58b1e2fc12d12174
Eli Ribble <eli@gleipnir.technology>
2025-10-14 23:17:16 -0700

    Make the sort page look better, add ascending.

--- Nidus/View/SortSelectionView.swift
+++ Nidus/View/SortSelectionView.swift
@@ -1,3 +1,4 @@
+import OSLog
 import SwiftUI
 
 enum NoteListSort {
@@ -8,15 +9,41 @@ enum NoteListSort {
 
 struct SortSelectionView: View {
 	@State var selection: Int = 0
+	@State var ascending: Bool = true
+	func apply() {
+		Logger.foreground.info("Apply sort")
+	}
 	var body: some View {
-		Picker(
-			selection: $selection,
-			label: Text("Note Sort"),
-			content: {
-				Text("Note age").tag(0)
-				Text("Distance from selected location").tag(1)
-				Text("Distance from user location").tag(2)
+		Form {
+			Picker(
+				selection: $selection,
+				label: Text("Sort notes by"),
+				content: {
+					Text("Note age").tag(0)
+					Text("Distance from selected location").tag(1)
+					Text("Distance from user location").tag(2)
+				}
+			)
+			Picker(
+				selection: $ascending,
+				label: Text("Ascending"),
+				content: {
+					Text("Ascending").tag(true)
+					Text("Descending").tag(false)
+				}
+			)
+		}.toolbar {
+			ToolbarItem(placement: .navigationBarTrailing) {
+				Button("Apply") {
+					apply()
+				}
 			}
-		)
+		}
+	}
+}
+
+#Preview {
+	NavigationStack {
+		SortSelectionView()
 	}
 }
